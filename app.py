5849bed42677166e86185e702d60ddca7f37d119
sagichnicht04 <florian.zuerner@gmail.com>
2024-05-27 18:14:38 +0200

    Mach mein Popup 	modified:   app.py 	new file:   game.py 	modified:   templates/index.html

--- app.py
+++ app.py
@@ -3,12 +3,16 @@ from flask_socketio import SocketIO, emit
 import subprocess
 
 app = Flask(__name__)
-socketio = SocketIO(app,debug=True,cors_allowed_origins='*',async_mode='eventlet')
+socketio = SocketIO(app,debug=True,cors_allowed_origins='*')
 
+
+@app.route('/')
+def main():
+    return render_template('index.html')
 
 @app.route('/lobby')
-def main():
-        return render_template('base.html')
+def lobby():
+    return render_template('lobby.html')
 
 @socketio.on("my_event")
 def checkping():
@@ -16,5 +20,8 @@ def checkping():
         cmd = 'ping -c 1 8.8.8.8|head -2|tail -1'
         listing1 = subprocess.run(cmd,stdout=subprocess.PIPE,text=True,shell=True)
         sid = request.sid
-        emit('server', {"data1":x, "data":listing1.stdout}, room=sid)
+        emit("server", {"data1":x, "data":listing1.stdout}, room=sid)
         socketio.sleep(1)
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000, debug=True)
