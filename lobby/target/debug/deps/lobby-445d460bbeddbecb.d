a302fb8ce0df9230277aa2050ba8e53d9642c4d2
sagichnicht04 <florian.zuerner@gmail.com>
2024-05-28 20:42:26 +0200

    Lobby funktioniert? 	new file:   .gitignore 	deleted:    __pycache__/lobby.cpython-310.pyc 	modified:   app.py 	new file:   lobby/Cargo.toml 	new file:   lobby/src/main.rs 	deleted:    test_crs.py

---
+++ lobby/target/debug/deps/lobby-445d460bbeddbecb.d
@@ -0,0 +1,5 @@
+/mnt/share/Development/VSCode/viererfieber/lobby/target/debug/deps/lobby-445d460bbeddbecb: src/main.rs
+
+/mnt/share/Development/VSCode/viererfieber/lobby/target/debug/deps/lobby-445d460bbeddbecb.d: src/main.rs
+
+src/main.rs:
