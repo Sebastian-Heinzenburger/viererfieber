879e93f01b4a06b2881f975adb7f3dc26fac85f4
sagichnicht04 <florian.zuerner@gmail.com>
2024-05-29 11:40:50 +0200

    Autofocus JUHUUU 	modified:   static/welcome.js

--- static/welcome.js
+++ static/welcome.js
@@ -1,54 +1,10 @@
-
-
-const socket = io(); //socketio connection to server//
-$(document).ready(function() {
-});
-socket.on("connect", () => {
-console.log("connected");
-      document.getElementById("header").innerHTML = "<h3>" + "Websocket Connected" + "</h3";
-});
-
-socket.on("disconnect", () => {
-console.log("disconnected");
-      document.getElementById("header").innerHTML = "<h3>" + "Websocket Disconnected" + "</h3>";
-});
-
-function myupdate() {
-//Event sent by Client
-socket.emit("my_event", function() {
-});
-}
-
-// Event sent by Server//
-socket.on("server", function(msg) {
-      let myvar = JSON.parse(msg.data1);
-      //Check if entire data is sent by server//
-      if (myvar == "4") {
-              document.getElementById("demo").innerHTML = "";
-              document.querySelector('#checkbutton').innerText = "Submit";
-              document.getElementById("checkbutton").style.cursor = "pointer";
-              document.getElementById("checkbutton").disabled = false;
-              document.getElementById("checkbutton").className = "btn btn-primary";
-
-      }
-
-      else {
-              document.getElementById("demo").innerHTML += msg.data + "<br>";
-              document.getElementById("checkbutton").disabled = true;
-              document.getElementById("checkbutton").innerHTML = "Loading..";
-              document.getElementById("checkbutton").style.cursor = "not-allowed";
-              document.getElementById("checkbutton").style.pointerEvents = "auto";
-      }
-});
-
-
-
 function createGame() {
     document.location.href = "/lobby";
   }
 
   function joinGame() {
     document.querySelector("#join_game_dialog").setAttribute("open", "open");
+    document.querySelector("#num0").focus();
   }
 
   function focusInput(actual, next) {
