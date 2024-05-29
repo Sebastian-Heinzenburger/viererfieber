bde882cdb1636b42000b5c38dd172fcf3c92d650
Tobias Lawall <tobias.lawall@sap.com>
2024-05-29 15:16:37 +0200

    This works perfectly fine 	modified:   static/lobby.css 	modified:   static/lobby.js 	modified:   templates/lobby.html

--- static/lobby.js
+++ static/lobby.js
@@ -1,4 +1,42 @@
 const socket = new WebSocket("/ws");
+let socket_open = false;
+
+//{"end":false,"own_name":"Bobby","field":[[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0]],"opponent_name":"Alice","own_ready":true,"opponent_ready": false,"turn":true}
+socket.onmessage = function (e) {
+    message_json = JSON.parse(e.data);
+    if(message_json.type == "Lobby"){
+        refreshLobby(message_json);
+    }
+};
+
+socket.onopen = function (e) {
+    socket_open = true;
+    obj = {};
+    obj.type = "init";
+    obj.code = document.querySelector("#table").style.filter = "invert(40%) grayscale(50%);";
+    socket.send(JSON.stringify(obj));
+}
+
+function refreshLobby(message_json){
+    if(message_json.end || !message_json.opponent_ready || !message_json.own_ready){
+        document.querySelector("#board").style.filter = "invert(40%) grayscale(50%);";
+        if(message_json.end){
+        document.querySelector("#join_game_dialog").showModal();
+            document.querySelector("#message").innerText = message_json.turn ? "gewonnen." : "verloren.";
+            document.querySelector("#join_game_dialog").showModal();
+        }
+    }
+    else{
+        document.querySelector("#board").style.filter = "";
+    }
+    document.querySelector("#code_input").value=message_json.lobby_code;
+    createTable(message_json.field);
+    if(message_json.enemy_player != ""){
+        document.querySelector("#opponent_character_name").value=message_json.enemy_player;
+    }
+    document.querySelector("#opponent_ready").checked = message_json.opponent_ready;
+
+}
 
 function readyForGame() {
     //namen auslesen
@@ -7,6 +45,7 @@ function readyForGame() {
 
     console.debug(nameInput.value);
 }
+createTable([[0,0,0,0,0,0,0],[0,0,0,1,0,0,0],[0,0,0,2,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,2,0]]);
 function createTable(board){
     let table = document.querySelector("#board");
     for (let i = 0; i < 6; i++) {
