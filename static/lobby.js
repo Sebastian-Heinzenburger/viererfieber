2bfd9df1f40b6dfc969910300d8067c137609d1e
sagichnicht04 <florian.zuerner@gmail.com>
2024-06-05 12:03:22 +0200

    Warum braucht der commit so lange 	modified:   static/lobby.css 	modified:   static/lobby.js 	modified:   templates/lobby.html

--- static/lobby.js
+++ static/lobby.js
@@ -1,6 +1,6 @@
 const socket = new WebSocket("/ws");
 let socket_open = false;
-
+let chip_falling_animation = false;
 
 const copyToClipboard = str => {
     if (navigator && navigator.clipboard && navigator.clipboard.writeText)
@@ -17,8 +17,19 @@ function clipboardInvitation(){
 
 document.addEventListener("mousemove", (e) => {
     let chip = document.querySelector("#chip");
-    chip.style.left = e.clientX - 50 + "px";
-    chip.style.top = e.clientY - 50 + "px";
+    if(!chip_falling_animation){
+        let table = document.querySelector("#table");
+        for (let i = 0; i < table.rows[0].cells.length; i++) {
+            let column = table.rows[0].cells[i];
+            let rect = column.getBoundingClientRect();
+        //  console.log(rect.top, rect.right, rect.bottom, rect.left);
+            
+            if(e.clientX>=rect.left && e.clientX<rect.right){
+                chip.style.top = (rect.top+globalThis.scrollY-chip.clientHeight/2)+"px";
+                chip.style.left = rect.left + "px";
+            }
+        }
+    }    
 });
 
 socket.onmessage = function (e) {
@@ -37,35 +48,59 @@ socket.onopen = function () {
     console.log("Geinited");
 }
 
-function updateName(){
+function updateName(){  
     let obj = {};
     obj.type = "setName";
     obj.name = document.querySelector("#own_character_name").value;
     socket.send(JSON.stringify(obj));
+
 }
 
-function clickCell(column){
+async function clickCell(column){
+    let chip = document.querySelector("#chip");
+    let table = document.querySelector("#table");
+    
+    chip_falling_animation = true;
+    for (let i = 0; i < table.rows.length; i++) {
+        await new Promise(r => setTimeout(r, 100));
+        let column_element = table.rows[i].cells[column];
+        let rect = column_element.getBoundingClientRect();
+        
+        if("darkslateblue" == column_element.firstChild.firstChild.getAttribute("fill")){
+            chip.style.top = (rect.top+globalThis.scrollY)+"px";
+            chip.style.left = rect.left + "px";
+        }
+        else{
+            break;
+        }
+    }
     let obj = {};
     obj.type = "drop";
     obj.column = column;
     socket.send(JSON.stringify(obj));
+    await new Promise(r => setTimeout(r, 500));
+    chip_falling_animation = false;
 }
 
 function refreshLobby(message_json){
     let status = document.querySelector("#status_section");
+    let parent_div = status.parentElement.parentElement;
     if(!message_json.opponent_ready || !message_json.own_ready){
         status.innerText = "Warten bis alle bereit sind..."
-        status.style.opacity = "0.5";
+        parent_div.style.opacity = "0.5";
         status.style.animation = "";
+        status.parentElement.onmouseover = (event) => {
+        };
+
     }
     else if(message_json.turn){
         status.innerText = "Du bist an der Reihe"
-        status.style.opacity = "0.8";
+        parent_div.style.opacity = "0.8";
         status.style.animation = "blink 2s step-start 0s infinite";
     }
     else{
         status.innerText = "Dein Gegner ist an der Reihe"
-        status.style.opacity = "0.5";
+        parent_div.style.opacity = "0.5";
         status.style.animation = "";
     }
 
@@ -74,8 +109,10 @@ function refreshLobby(message_json){
         document.querySelector("#table").setAttribute("style", "filter: invert(40%) grayscale(50%)");
         if(message_json.end){
             status.innerText = message_json.turn ? "٩(＾◡＾)۶ Gewonnen " : "༼ ༎ຶ ᆺ ༎ຶ༽ Verloren";
-            status.style.opacity = "1";
+            parent_div.style.opacity = "1";
             status.style.animation = "blink 0.5s step-start 0s infinite";
+            status.setAttribute("onclick","window.location.href='"+document.location.protocol + "//" + document.location.host+"';");
+            status.style.cursor = "pointer";
         }
     }
     else{
@@ -123,17 +160,7 @@ function createTable(server_field){
                 default: 
                     color = "darkslateblue";
             }
-            td.innerHTML = '<svg height="100" width="100" xmlns="http://www.w3.org/2000/svg"><circle r="45" cx="50" cy="50" fill="'+color+'" /></svg>';
-            //   let svg = document.createElement("svg");
-            //   svg.setAttribute("height", "100");
-            //   svg.setAttribute("width", "100");
-            //   svg.setAttribute("xmlns", "http://www.w3.org/2000/svg")
-            //   let circle = document.createElement("circle");
-            //   circle.setAttribute("r", 45);
-            //   circle.setAttribute("fill", "blue")
-            //   svg.appendChild(circle);
-            // svg.innerHTML = '<circle r="45" cx="50" cy="50" fill="green" />'
-            //   td.appendChild(svg);
+            td.innerHTML = '<svg height="100" width="100"><circle r="45" cx="50" cy="50" fill="'+color+'" /></svg>';
         }
     }
 }
